47daba8ac9a6983e708ef37670fed0b333a4b73e
Brandon Herrin <brandon.herrin@live.com>
2022-10-15 12:21:53 -0600

    Completed Module 5

--- server.js
+++ server.js
@@ -21,19 +21,22 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-})
-passport.deserializeUser((id, done) => {
-  // myDB.findOne({_id: new ObjectID(id)}, (err, doc) => {
-  //   done(null, null);
-  // })
-  done(null, null);
-})
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
 
-app.route('/').get((req, res) => {
-  res.render('./pug/index', {title: 'Hello', message: 'Please login'});
-});
+  app.route('/').get((req, res) => {
+    res.render('./pug/index', {title: 'Connected to Database', message: 'Please login'});
+  });
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  })
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({_id: new ObjectID(id)}, (err, doc) => {
+      done(null, doc);
+    })
+    // done(null, null);
+  });
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
