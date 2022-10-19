d7b0e5dfc75773d9f7a0d43b10a5aa51eab1b35f
Brandon Herrin <brandon.herrin@live.com>
2022-10-18 22:59:08 -0600

    Completed module 20.

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const session = require("express-session");
 const passport = require("passport");
 const routes = require("./routes");
 const auth = require("./auth");
+const passportSocketIo = require("passport.socketio");
+const cookieParser = require("cookie-parser");
+const MongoStore = require("connect-mongo")(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
 
 const app = express();
 const http = require("http").createServer(app);
@@ -23,10 +28,22 @@ app.use(
     resave: true,
     saveUninitialized: true,
     cookie: { secure: false },
+    key: "express.sid",
+    store: store,
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: "express.sid",
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail,
+  })
+);
 myDB(async (client) => {
   const myDataBase = await client.db("database").collection("users");
   routes(app, myDataBase);
@@ -36,6 +53,7 @@ myDB(async (client) => {
     console.log("A user has connected.");
     ++currentUsers;
     io.emit("user count", currentUsers);
+    console.log(`user ${socket.request.user.name} connected`);
     socket.on("disconnect", () => {
       console.log("A user has disconnected.");
       --currentUsers;
@@ -50,6 +68,16 @@ myDB(async (client) => {
     res.render("pug", { title: err, message: "Unable to login" });
   });
 });
+
+function onAuthorizeSuccess(data, accept) {
+  console.log("successful connection to socket.io");
+  accept(null, true);
+}
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log("failed connection to socket.io:", message);
+  accept(null, false);
+}
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
